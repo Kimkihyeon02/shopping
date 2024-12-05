176b7a0f03e09bfd3c8f8cf51e3c4cb2443cb6f7
Kimkihyeon02 <notch910@naver.com>
2024-12-05 17:03:44 +0900

    1st

---
+++ shopping/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+
+      <Button variant={'default'} size={'lg'}>
+        Button:click me
+      </Button>
+    </div>
+  )
+}
